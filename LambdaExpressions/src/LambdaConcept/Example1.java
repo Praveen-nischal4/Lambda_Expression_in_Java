1a23b347a315e0bbf45ae0e9198d1a003a6b7b30
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-08 23:47:30 +0000

    Prog 1

---
+++ LambdaExpressions/src/LambdaConcept/Example1.java
@@ -0,0 +1,21 @@
+package LambdaConcept;
+
+@FunctionalInterface
+interface Addition
+{
+	abstract void add(int a,int b);
+}
+
+
+public class Example1 {
+
+	public static void main(String[] args) {
+
+    int a=7,b=8;
+    
+    Addition ad = (x,y) -> {  System.out.println("Sum ="+ (x+y)); };
+    	 	
+     ad.add(a, b);        //calling lambda Expression
+	}
+
+}
